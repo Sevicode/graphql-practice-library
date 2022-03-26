77cbbeb73846ea8634b57aba8e87f1f34f16e0c2
Sevda <sevda.amini@gmail.com>
2022-03-27 00:34:24 +1000

    database connected

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
+const mongoose = require('mongoose')
 const app = express()
+require('dotenv').config();
+const db = process.env.MONGO_URI
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.log(err));
 app.use('/graphql', graphqlHTTP({
     // can be written schema:schema which means schema property equals to the schema we just created
     // but because the names are the same based on ES6 rules we can just write schema.
