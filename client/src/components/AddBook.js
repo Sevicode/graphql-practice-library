c8b8ce14d8384d926be957744cd7893de079a1f7
Sevda <sevda.amini@gmail.com>
2022-03-28 00:26:35 +1000

    queries component added

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,55 +1,46 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
-
+import { useQuery } from "@apollo/client";
+import { getAuthorsQuery } from "../queries/queries";
 
 function AddBook() {
-    const {loading, error, data} = useQuery(getAuthorsQuery)
-    const displayAuthors = () => {
-        if (loading) {
-            return <p>Loading...</p>
-            
-        }
-        if (error) {
-            return <p>oops! something went wrong!</p>
-            
-        }else{
-            return data.authors.map((author) => {
-                return(<option key={author.id} value={author.id}>{author.name}</option>)
-            })
-            
-        }
+  const { loading, error, data } = useQuery(getAuthorsQuery);
+  const displayAuthors = () => {
+    if (loading) {
+      return <p>Loading...</p>;
     }
-   
-    return (
-        <form id="add-book">
-        <div className="field">
-            <label>Book name:</label>
-            <input type="text" />
-        </div>
-        <div className="field">
-            <label>Genre:</label>
-            <input type="text" />
-        </div>
-        <div className="field">
-            <label>Author:</label>
-            <select>
-                <option>Select author</option>
-                { displayAuthors() }
-            </select>
-        </div>
-        <button>Add Book</button>
+    if (error) {
+      return <p>oops! something went wrong!</p>;
+    } else {
+      return data.authors.map((author) => {
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>
+        );
+      });
+    }
+  };
 
+  return (
+    <form id="add-book">
+      <div className="field">
+        <label>Book name:</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label>Genre:</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label>Author:</label>
+        <select>
+          <option>Select author</option>
+          {displayAuthors()}
+        </select>
+      </div>
+      <button>Add Book</button>
     </form>
-    );
-  }
-  
-  export default AddBook;
+  );
+}
+
+export default AddBook;
---
+++ client/src/queries/queries.js
@@ -0,0 +1,20 @@
+import { gql} from "@apollo/client";
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+export {getAuthorsQuery, getBooksQuery};
