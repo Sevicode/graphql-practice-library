ff5717c747b0cf394b97b1c34a21e9cecac0879b
Sevda <sevda.amini@gmail.com>
2022-03-27 22:47:30 +1000

    created components and added Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import BookList from "./componnets/BookList";
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's reading list</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
