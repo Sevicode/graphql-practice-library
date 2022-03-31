2120d40f31939d507d1ba4ecf33786cd610500ee
Sevda <sevda.amini@gmail.com>
2022-04-01 00:09:59 +1000

    displaying book details

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -4,12 +4,26 @@ import {getBookQuery} from "../queries/queries"
 
 
 
-function BookDetails() {
- 
+function BookDetails({ bookId }) {
+const {loading, error, data} = useQuery(getBookQuery, {
+    variables: { id: bookId }
+    });
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error!</p>
+    const {book} = data;
 
   return (
     <>
-    <div id="book"></div>
+    <div id="book">
+        <h2>{book.name}</h2>
+        <p>{book.genre}</p>
+        <p>{book.author.name}</p>
+        <p>All books by this author:</p>
+        <ul>
+            {book.author.books.map(book => <li key={book.id}>{book.name}</li>)}
+        </ul>
+        
+    </div>
      
       
     </>
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useQuery } from "@apollo/client";
 import {getBooksQuery} from "../queries/queries"
 import BookDetails from "./BookDetails";
@@ -7,6 +7,10 @@ import BookDetails from "./BookDetails";
 
 function BookList() {
   const { loading, error, data } = useQuery(getBooksQuery);
+  const [selected, setSelected] = useState(null);
+
+  
+
   const displayBooks = () => {
     if (loading) {
       return <p>Loading...</p>;
@@ -15,15 +19,17 @@ function BookList() {
       return <p>oops! Something went wrong!</p>;
     } else {
       return data.books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
+        return <li key={book.id} onClick={(e) => {setSelected(book.id)}}>{book.name}</li>;
       });
     }
   };
 
+ 
+
   return (
     <>
-      <ul>{displayBooks()}</ul>
-      <BookDetails />
+      <ul id="book-list">{displayBooks()}</ul>
+      {selected ? <BookDetails bookId={selected} />:<div>No book selected</div>}
       
     </>
   );
